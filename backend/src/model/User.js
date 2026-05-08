ff86b18a5ff91c07997f2ead8fb24ca912f2e911
Krishna <srikrishna11420@gmail.com>
2026-05-08 11:27:59 +0530

    Backend updation

---
+++ backend/src/model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    organization: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Organization'
+    }
+}, {
+    timestamps: true
+});
+module.exports = mongoose.model('User', userSchema);

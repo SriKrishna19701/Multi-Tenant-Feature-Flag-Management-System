7f6e3566f4578d0c205c570eee9e4f21c533832f
Krishna <srikrishna11420@gmail.com>
2026-05-09 12:30:36 +0530

    Backend updation

---
+++ backend/src/controller/organizationController.js
@@ -0,0 +1,31 @@
+
+const Organization = require('../models/Organization');
+
+// Create a new organization
+exports.createOrganization = async (req, res) => {
+    try {
+        const { name } = req.body;
+            if (!name) {
+            return res.status(400).json({ message: 'Organization name is required' });
+        }
+        const existingOrg = await Organization.findOne({ name });
+        if (existingOrg) {
+            return res.status(400).json({ message: 'Organization already exists' });
+        }
+        const organization = new Organization({ name });
+        await organization.save();
+        res.status(201).json(organization);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Get all organizations
+exports.getOrganizations = async (req, res) => {
+    try {
+        const organizations = await Organization.find();
+        res.json(organizations);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};

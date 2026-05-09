ed1546e8de34589b0069811a3a35d71ab752fc07
Krishna <srikrishna11420@gmail.com>
2026-05-09 13:24:11 +0530

    Backend updation

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const organizationRoutes = require('./routes/organizationRoutes');
+const featureRoutes = require('./routes/featureRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/organizations', organizationRoutes);
+app.use('/api/features', featureRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
